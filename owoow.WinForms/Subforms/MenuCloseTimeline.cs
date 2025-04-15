084ac48516c9e70617e24a680816027f2a549dac
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2025-04-16 08:56:12 +1000

    Uncouple "Holding Direction" for subwindow

--- owoow.WinForms/Subforms/MenuCloseTimeline.cs
+++ owoow.WinForms/Subforms/MenuCloseTimeline.cs
@@ -20,7 +20,6 @@ public partial class MenuCloseTimeline : Form
         f.SetTextBoxText(f.TB_Seed1.Text, TB_Seed1);
         f.SetTextBoxText(string.IsNullOrEmpty(f.TB_CurrentAdvances.Text) ? "0" : f.TB_CurrentAdvances.Text.Replace(",", string.Empty), TB_Timeline_Initial);
         f.SetTextBoxText(((TextBox)f.Controls.Find($"TB_{Tab}_NPCs", true).FirstOrDefault()!).Text, TB_Timeline_NPCs);
-        f.SetCheckBoxCheckedState(((CheckBox)f.Controls.Find($"CB_{Tab}_MenuClose_Direction", true).FirstOrDefault()!).Checked, CB_Timeline_MenuClose_Direction);
         f.SetComboBoxSelectedIndex(CB_Timeline_Weather.Items.IndexOf($"{((ComboBox)f.Controls.Find($"CB_{Tab}_Weather", true).FirstOrDefault()!).SelectedItem}"), CB_Timeline_Weather);
 
         TB_Seed0.KeyPress += new KeyPressEventHandler(f.KeyPress_AllowOnlyHex!);
@@ -77,7 +76,6 @@ public partial class MenuCloseTimeline : Form
     private void MenuCloseTimeline_FormClosing(object sender, FormClosingEventArgs e)
     {
         MainWindow.MenuCloseTimelineFormOpen = false;
-        MainWindow.SetCheckBoxCheckedState(CB_Timeline_MenuClose_Direction.Checked, (CheckBox)MainWindow.Controls.Find($"CB_{Tab}_MenuClose_Direction", true).FirstOrDefault()!);
         MainWindow.SetTextBoxText(TB_Timeline_NPCs.Text, (TextBox)MainWindow.Controls.Find($"TB_{Tab}_NPCs", true).FirstOrDefault()!);
     }
 
