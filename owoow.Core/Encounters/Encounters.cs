4e3844b4128253b4fbde0b83c075629cf03b7f84
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2025-06-27 23:54:02 +1000

    Filter Encounter Tables based on possible weathers
    Closes #5

--- owoow.Core/Encounters/Encounters.cs
+++ owoow.Core/Encounters/Encounters.cs
@@ -1,3 +1,4 @@
+using owoow.Core.Enums;
 using owoow.Core.Interfaces;
 using System.Collections.Immutable;
 using System.Text.Json;
@@ -45,6 +46,7 @@ public static class Encounters
         {
             foreach (var weather in area.Value)
             {
+                if ((WeatherNameToValue(weather.Key) & WeathersByArea[area.Key]) == 0) continue; // Filter out impossible weathers
                 foreach (var enc in weather.Value.Encounters!)
                 {
                     var species = enc.Value.Species!;
@@ -85,6 +87,7 @@ public static class Encounters
         {
             foreach (var weather in area.Value)
             {
+                if ((WeatherNameToValue(weather.Key) & WeathersByArea[area.Key]) == 0) continue; // Filter out impossible weathers
                 foreach (var enc in weather.Value.Encounters!)
                 {
                     var species = enc.Value.Species!;
@@ -116,6 +119,7 @@ public static class Encounters
         {
             foreach (var weather in area.Value)
             {
+                if ((WeatherNameToValue(weather.Key) & WeathersByArea[area.Key]) == 0) continue; // Filter out impossible weathers
                 foreach (var enc in weather.Value.Encounters!)
                 {
                     var species = enc.Value.Species!;
@@ -155,6 +159,7 @@ public static class Encounters
         {
             foreach (var weather in area.Value)
             {
+                if ((WeatherNameToValue(weather.Key) & WeathersByArea[area.Key]) == 0) continue; // Filter out impossible weathers
                 foreach (var enc in weather.Value.Encounters!)
                 {
                     var species = enc.Value.Species!;
@@ -251,17 +256,17 @@ public static class Encounters
     {
         "Sword" => tabName switch
         {
-            "Fishing" => [.. SwordFishing![area].Keys],
-            "Hidden" => [.. SwordHidden![area].Keys],
-            "Static" => [.. SwordStatic![area].Keys],
-            _ => [.. SwordSymbol![area].Keys],
+            "Fishing" => [.. SwordFishing![area].Keys.Where(weather => (WeatherNameToValue(weather) & WeathersByArea[area]) != 0)],
+            "Hidden"  => [.. SwordHidden![area].Keys.Where(weather => (WeatherNameToValue(weather) & WeathersByArea[area]) != 0)],
+            "Static"  => [.. SwordStatic![area].Keys.Where(weather => (WeatherNameToValue(weather) & WeathersByArea[area]) != 0)],
+            _         => [.. SwordSymbol![area].Keys.Where(weather => (WeatherNameToValue(weather) & WeathersByArea[area]) != 0)],
         },
         _ => tabName switch
         {
-            "Fishing" => [.. ShieldFishing![area].Keys],
-            "Hidden" => [.. ShieldHidden![area].Keys],
-            "Static" => [.. ShieldStatic![area].Keys],
-            _ => [.. ShieldSymbol![area].Keys],
+            "Fishing" => [.. ShieldFishing![area].Keys.Where(weather => (WeatherNameToValue(weather) & WeathersByArea[area]) != 0)],
+            "Hidden" => [.. ShieldHidden![area].Keys.Where(weather => (WeatherNameToValue(weather) & WeathersByArea[area]) != 0)],
+            "Static" => [.. ShieldStatic![area].Keys.Where(weather => (WeatherNameToValue(weather) & WeathersByArea[area]) != 0)],
+            _ => [.. ShieldSymbol![area].Keys.Where(weather => (WeatherNameToValue(weather) & WeathersByArea[area]) != 0)],
         },
     };
 
@@ -296,4 +301,151 @@ public static class Encounters
         }
         return ret.AsReadOnly();
     }
+
+    private static AvailableWeather WeatherNameToValue(string name) => name switch
+    {
+        "Normal Weather" => AvailableWeather.Normal,
+        "Overcast" => AvailableWeather.Overcast,
+        "Raining" => AvailableWeather.Raining,
+        "Thunderstorm" => AvailableWeather.Thunderstorm,
+        "Intense Sun" => AvailableWeather.Intense_Sun,
+        "Snowing" => AvailableWeather.Snowing,
+        "Snowstorm" => AvailableWeather.Snowstorm,
+        "Sandstorm" => AvailableWeather.Sandstorm,
+        "Heavy Fog" => AvailableWeather.Heavy_Fog,
+        _ => AvailableWeather.None,
+    };
+
+    // Adapted from https://github.com/kwsch/pkNX/blob/master/FlatBuffers/SWSH/Gen8/Wild/EncounterTableUtil.cs#L299
+    private static readonly Dictionary<string, AvailableWeather> WeathersByArea = new()
+    {
+        { "Axew's Eye", AvailableWeather.All },
+        { "Ballimere Lake", AvailableWeather.All_CT },
+        { "Ballimere Lake (Surfing)", AvailableWeather.All_Ballimere },
+        { "Brawlers' Cave", AvailableWeather.Normal },
+        { "Brawlers' Cave (Surfing)", AvailableWeather.Normal },
+        { "Bridge Field", AvailableWeather.All },
+        { "Bridge Field (Flying)", AvailableWeather.All },
+        { "Bridge Field (Surfing)", AvailableWeather.All },
+        { "Challenge Beach", AvailableWeather.All_IoA },
+        { "Challenge Beach (Beach)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Challenge Beach (Surfing)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Challenge Road", AvailableWeather.All_IoA },
+        { "City of Motostoke", AvailableWeather.Normal },
+        { "Courageous Cavern", AvailableWeather.Normal },
+        { "Courageous Cavern (Surfing)", AvailableWeather.Normal },
+        { "Dappled Grove", AvailableWeather.All },
+        { "Dusty Bowl", AvailableWeather.All },
+        { "Dusty Bowl (Flying)", AvailableWeather.All },
+        { "Dusty Bowl and Giant's Mirror (Surfing)", AvailableWeather.All },
+        { "Dyna Tree Hill", AvailableWeather.Normal },
+        { "East Lake Axewell", AvailableWeather.All },
+        { "East Lake Axewell (Flying)", AvailableWeather.All },
+        { "East Lake Axewell (Surfing)", AvailableWeather.All },
+        { "Fields of Honor", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Fields of Honor (Beach)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Fields of Honor (Surfing)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Forest of Focus", AvailableWeather.All_IoA },
+        { "Forest of Focus (Surfing)", AvailableWeather.All_IoA },
+        { "Frigid Sea", AvailableWeather.No_Sun_Sand },
+        { "Frigid Sea (Surfing)", AvailableWeather.No_Sun_Sand },
+        { "Frostpoint Field", AvailableWeather.All_CT },
+        { "Galar Mine", AvailableWeather.Normal },
+        { "Galar Mine No. 2", AvailableWeather.Normal },
+        { "Giant's Bed", AvailableWeather.All_CT },
+        { "Giant's Bed / Giant's Foot (Surfing)", AvailableWeather.All_CT },
+        { "Giant's Cap", AvailableWeather.All },
+        { "Giant's Cap (2)", AvailableWeather.All },
+        { "Giant's Cap (3)", AvailableWeather.All },
+        { "Giant's Cap (Ground)", AvailableWeather.All },
+        { "Giant's Cap (Lunatone/Solrock)", AvailableWeather.All },
+        { "Giant's Foot", AvailableWeather.All_CT },
+        { "Giant's Mirror", AvailableWeather.All },
+        { "Giant's Mirror (Flying)", AvailableWeather.All },
+        { "Giant's Mirror (Ground)", AvailableWeather.All },
+        { "Giant's Seat", AvailableWeather.All },
+        { "Glimwood Tangle", AvailableWeather.Normal },
+        { "Hammerlocke Hills", AvailableWeather.All },
+        { "Hammerlocke Hills (Flying)", AvailableWeather.All },
+        { "Honeycalm Island", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Honeycalm Island (Surfing)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Honeycalm Sea", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Honeycalm Sea (Sharpedo)", AvailableWeather.All_IoA },
+        { "Honeycalm Sea (Surfing)", AvailableWeather.All_IoA },
+        { "Insular Sea", AvailableWeather.Normal },
+        { "Insular Sea (Sharpedo)", AvailableWeather.Normal },
+        { "Insular Sea (Surfing)", AvailableWeather.Normal },
+        { "Lake of Outrage", AvailableWeather.All },
+        { "Lake of Outrage (Surfing)", AvailableWeather.All },
+        { "Lakeside Cave", AvailableWeather.All_Ballimere },
+        { "Loop Lagoon", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Loop Lagoon (Beach)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Loop Lagoon (Surfing)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Motostoke Outskirts", AvailableWeather.Normal },
+        { "Motostoke Riverbank", AvailableWeather.All },
+        { "Motostoke Riverbank (Surfing)", AvailableWeather.All },
+        { "North Lake Miloch", AvailableWeather.All },
+        { "North Lake Miloch (Surfing)", AvailableWeather.All },
+        { "Old Cemetery", AvailableWeather.All_CT },
+        { "Path to the Peak", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Intense_Sun | AvailableWeather.Icy | AvailableWeather.Heavy_Fog },
+        { "Potbottom Desert", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Raining | AvailableWeather.Sandstorm | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Roaring-Sea Caves", AvailableWeather.All_CT },
+        { "Roaring-Sea Caves (Surfing)", AvailableWeather.Overcast },
+        { "Rolling Fields", AvailableWeather.All },
+        { "Rolling Fields (2)", AvailableWeather.All },
+        { "Rolling Fields (Flying)", AvailableWeather.All },
+        { "Rolling Fields (Ground)", AvailableWeather.All },
+        { "Route 1", AvailableWeather.Normal },
+        { "Route 2", AvailableWeather.Normal },
+        { "Route 2 (High Level)", AvailableWeather.Normal },
+        { "Route 2 (Surfing)", AvailableWeather.Normal },
+        { "Route 3", AvailableWeather.Normal },
+        { "Route 3 (Garbage)", AvailableWeather.Normal },
+        { "Route 4", AvailableWeather.Normal },
+        { "Route 5", AvailableWeather.Normal },
+        { "Route 6", AvailableWeather.Intense_Sun },
+        { "Route 7", AvailableWeather.Normal },
+        { "Route 8", AvailableWeather.Snowing },
+        { "Route 8 (on Steamdrift Way)", AvailableWeather.Snowing },
+        { "Route 9", AvailableWeather.Snowing },
+        { "Route 9  (Surfing)", AvailableWeather.Snowing },
+        { "Route 9 (in Circhester Bay)", AvailableWeather.Snowing },
+        { "Route 9 (in Circhester Bay) (Surfing)", AvailableWeather.Snowing },
+        { "Route 9 (in Outer Spikemuth)", AvailableWeather.Overcast },
+        { "Route 10", AvailableWeather.Snowstorm },
+        { "Route 10 (Near Station)", AvailableWeather.Snowstorm },
+        { "Slippery Slope", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Intense_Sun | AvailableWeather.Icy | AvailableWeather.Heavy_Fog },
+        { "Slumbering Weald", AvailableWeather.Normal },
+        { "Slumbering Weald (High Level)", AvailableWeather.Normal },
+        { "Slumbering Weald (Low Level)", AvailableWeather.Normal },
+        { "Snowslide Slope", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Intense_Sun | AvailableWeather.Icy | AvailableWeather.Heavy_Fog },
+        { "Soothing Wetlands", AvailableWeather.All_IoA },
+        { "Soothing Wetlands (Puddles)", AvailableWeather.All_IoA },
+        { "South Lake Miloch", AvailableWeather.All },
+        { "South Lake Miloch (2)", AvailableWeather.All },
+        { "South Lake Miloch (Flying)", AvailableWeather.All },
+        { "South Lake Miloch (Surfing)", AvailableWeather.All },
+        { "Stepping-Stone Sea", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Stepping-Stone Sea (Sharpedo)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Stepping-Stone Sea (Surfing)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Stony Wilderness", AvailableWeather.All },
+        { "Stony Wilderness (2)", AvailableWeather.All },
+        { "Stony Wilderness (3)", AvailableWeather.All },
+        { "Stony Wilderness (Flying)", AvailableWeather.All },
+        { "Three-Point Pass", AvailableWeather.All_CT },
+        { "Town of Hulbury", AvailableWeather.Normal },
+        { "Training Lowlands", AvailableWeather.All_IoA },
+        { "Training Lowlands (Beach)", AvailableWeather.All_IoA },
+        { "Training Lowlands (Surfing)", AvailableWeather.All_IoA },
+        { "Tunnel to the Top", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Intense_Sun | AvailableWeather.Icy | AvailableWeather.Heavy_Fog },
+        { "Turffield", AvailableWeather.Normal },
+        { "Warm-up Tunnel", AvailableWeather.All_IoA },
+        { "Watchtower Ruins", AvailableWeather.All },
+        { "Watchtower Ruins (Flying)", AvailableWeather.All },
+        { "West Lake Axewell", AvailableWeather.All },
+        { "West Lake Axewell (Surfing)", AvailableWeather.All },
+        { "Workout Sea", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Workout Sea (Sharpedo)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+        { "Workout Sea (Surfing)", AvailableWeather.Normal | AvailableWeather.Overcast | AvailableWeather.Stormy | AvailableWeather.Intense_Sun | AvailableWeather.Heavy_Fog },
+    };
 }
--- owoow.Core/Enums/WeatherType.cs
+++ owoow.Core/Enums/WeatherType.cs
@@ -1,4 +1,4 @@
-﻿namespace owoow.Core.Enums;
+namespace owoow.Core.Enums;
 
 public enum WeatherType
 {
@@ -13,3 +13,32 @@ public enum WeatherType
     Sandstorm,
     HeavyFog,
 };
+
+
+// From https://github.com/kwsch/pkNX/blob/master/FlatBuffers/SWSH/Gen8/Wild/EncounterTableUtil.cs#L144
+[Flags]
+#pragma warning disable RCS1154 // Sort enum members.
+public enum AvailableWeather
+#pragma warning restore RCS1154 // Sort enum members.
+{
+    None = 0,
+    Normal = 1,
+    Overcast = 1 << 1,
+    Raining = 1 << 2,
+    Thunderstorm = 1 << 3,
+    Intense_Sun = 1 << 4,
+    Snowing = 1 << 5,
+    Snowstorm = 1 << 6,
+    Sandstorm = 1 << 7,
+    Heavy_Fog = 1 << 8,
+    Shaking_Trees = 1 << 9,
+    Fishing = 1 << 10,
+
+    All = Normal | Overcast | Raining | Thunderstorm | Intense_Sun | Snowing | Snowstorm | Sandstorm | Heavy_Fog,
+    Stormy = Raining | Thunderstorm,
+    Icy = Snowing | Snowstorm,
+    All_IoA = Normal | Overcast | Stormy | Intense_Sun | Sandstorm | Heavy_Fog,         // IoA can have everything but snow
+    All_CT = Normal | Overcast | Stormy | Intense_Sun | Icy | Heavy_Fog,                // CT can have everything but sand
+    No_Sun_Sand = Normal | Overcast | Stormy | Icy | Heavy_Fog,                         // Everything but sand and sun
+    All_Ballimere = Normal | Overcast | Stormy | Intense_Sun | Snowing | Heavy_Fog,     // All Ballimere Lake weather
+}
